1f2e73c76f568cf8d751a4628c12b3e950d933e5
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-16 19:58:42 +0600

    api setup with axios

---
+++ src/services/api.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+});
+
+// Interceptor to add the auth token to every request if it exists
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem('accessToken');
+    if (token) {
+      config.headers['Authorization'] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default api;
+
