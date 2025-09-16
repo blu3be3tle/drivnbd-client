37831af3515d39505084aa4bc8de166956e617c2
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-16 17:45:53 +0600

    bug fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link } from 'react-router';
 
 // Placeholder data - replace with API calls
 const featuredProducts = [
