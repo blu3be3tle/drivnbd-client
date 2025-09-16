77344f66bd713dbf50beda28cad2fe0dbdc2ed35
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-16 17:32:38 +0600

    routes defined

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -4,7 +4,14 @@ import { Route } from 'react-router';
 const AppRoutes = () => {
     return (
         <Route>
-
+            <Route element={<MainLayout />}>
+                <Route path="/" element={<HomePa />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/contact" element={<ContactPage />} />
+            </Route>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/register" element={<RegisterPage />} />
+            <Route path="*" element={<NotFoundPage />} />
         </Route>
     );
 };
