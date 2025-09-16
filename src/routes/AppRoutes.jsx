45589e0f3fb5144855e404954db7e7786193bd54
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-16 16:07:45 +0600

    project setup

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Route } from 'react-router';
+
+const AppRoutes = () => {
+    return (
+        <Route>
+
+        </Route>
+    );
+};
+
+export default AppRoutes;
