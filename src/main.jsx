2d2e6ae2a0b69d39443b902519da146f860ff24c
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-09-16 21:14:39 +0600

    bug fix

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router'
-import AppRoutes from './routes/AppRoutes'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { AuthProvider } from './contexts/AuthContext.jsx';
+import { CartProvider } from './contexts/CartContext.jsx';
+import AppRoutes from './routes/AppRoutes.jsx';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <BrowserRouter>
-      <AppRoutes />
+      <AuthProvider>
+        <CartProvider>
+          <AppRoutes />
+        </CartProvider>
+      </AuthProvider>
     </BrowserRouter>
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,14 @@
-import React from 'react';
-import { Route } from 'react-router';
+import { Route, Routes } from 'react-router';
 import Home from '../pages/Home';
 import MainLayout from '../Layouts/MainLayout';
 
 const AppRoutes = () => {
     return (
-        <Route>
+        <Routes>
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
-                <Route path="/about" element={<AboutPage />} />
-                <Route path="/contact" element={<ContactPage />} />
             </Route>
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/register" element={<RegisterPage />} />
-            <Route path="*" element={<NotFoundPage />} />
-        </Route>
+        </Routes>
     );
 };
 
