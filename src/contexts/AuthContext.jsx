566328cebf8455423573ae1f48bbf120614dbe71
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-13 19:46:58 +0600

    authContext and cartContext setup

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -1,50 +1,14 @@
-import React, { createContext, useState, useEffect } from 'react';
-import api from '../services/api.js';
-import { jwtDecode } from 'jwt-decode';
+import { createContext } from "react";
+import useAuth from "../hooks/useAuth";
 
-const AuthContext = createContext(null);
+const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
-    const [user, setUser] = useState(null);
-    const [loading, setLoading] = useState(true);
-
-    useEffect(() => {
-        const accessToken = localStorage.getItem('accessToken');
-        if (accessToken) {
-            try {
-                const decodedUser = jwtDecode(accessToken);
-                if (decodedUser.exp * 1000 > Date.now()) {
-                    setUser(decodedUser);
-                } else {
-                    localStorage.removeItem('accessToken');
-                }
-            } catch {
-                localStorage.removeItem('accessToken');
-            }
-        }
-        setLoading(false);
-    }, []);
-
-    const login = async (email, password) => {
-        const response = await api.post('/auth/jwt/create/', { email, password });
-        const { access } = response.data;
-        localStorage.setItem('accessToken', access);
-        const decodedUser = jwtDecode(access);
-        setUser(decodedUser);
-    };
-
-    const logout = () => {
-        setUser(null);
-        localStorage.removeItem('accessToken');
-    };
-
-    const value = { user, login, logout, loading };
+    const allContext = useAuth();
 
     return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
+        <AuthContext.Provider value={allContext}>{children}</AuthContext.Provider>
     );
 };
 
-export { AuthContext };
+export default AuthContext;
