befa7484e899a1a53a85d587f090ad4337c5bd31
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-16 21:47:31 +0600

    Banner section moved to a new component

---
+++ src/components/Home/Banner/Banner.jsx
@@ -0,0 +1,28 @@
+
+
+const Banner = () => {
+    return (
+        <div>
+            <div
+                className="hero min-h-screen"
+                style={{
+                    backgroundImage:
+                        "url(https://i.postimg.cc/FHtPHRdk/handsome-tourist-sunglasses-straw-hat-looking-aside-smiling-pleased-exploring-sighsteeing.jpg)",
+                }}
+            >
+                <div className="hero-overlay"></div>
+                <div className="hero-content text-neutral-content text-center">
+                    <div className="max-w-2xl mb-32">
+                        <h1 className="mb-5 text-5xl font-bold">Summer Collection is Here</h1>
+                        <p className="mb-5">
+                            Discover the latest trends and refresh your wardrobe with our stunning new arrivals.
+                        </p>
+                        <button className="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gray-900 hover:bg-gray-700">Shop Now</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
