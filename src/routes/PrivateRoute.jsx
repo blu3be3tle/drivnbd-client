95f404906f66c1bbb63cfee52ff3b65e3e2b01f8
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-18 21:39:20 +0600

    unauth users are redirected to login page if try to access private routes

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -1,10 +1,19 @@
+import { useEffect, useState } from "react";
 import useAuthContext from "../hooks/useAuthContext";
 import { Navigate } from "react-router";
 
 const PrivateRoute = ({ children }) => {
     const { user } = useAuthContext();
-    if (user === null) return <p>Loading...</p>;
-    return user ? children : <Navigate to="/login"></Navigate>;
+    const [checking, setChecking] = useState(true);
+
+    useEffect(() => {
+        const timer = setTimeout(() => setChecking(false), 3000);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (checking && user === null) return <p>Loading...</p>;
+    if (!checking && user === null) return <Navigate to="/login" replace />;
+    return children;
 };
 
 export default PrivateRoute;
