39c058618b38558e955729cf26445920f80714d6
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-18 07:46:50 +0600

    shop page bg changed

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -2,7 +2,7 @@ import AllProducts from "../components/Shop/AllProducts";
 
 const Products = () => {
     return (
-        <div>
+        <div className="min-h-screen bg-gray-100 py-10">
             <AllProducts />
         </div>
     );
