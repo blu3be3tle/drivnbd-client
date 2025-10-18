5a5a58b4f75dcca14d5f226f97448dbe862ebebf
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-18 15:55:33 +0600

    login url created

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router';
 import Home from '../pages/Home';
 import MainLayout from '../Layouts/MainLayout';
 import Products from '../pages/Products';
+import Login from '../pages/Login';
 
 const AppRoutes = () => {
     return (
         <Routes>
             <Route element={<MainLayout />}>
                 <Route path="/" element={<Home />} />
+                <Route path='/login' element={<Login />} />
                 <Route path="/products" element={<Products />} />
             </Route>
         </Routes>
