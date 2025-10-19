2bf9b85a560d320c31707fb8e95fc4a02ab40649
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-19 19:38:06 +0600

    navbar cart icon linked to cart page

--- src/Layouts/Navbar.jsx
+++ src/Layouts/Navbar.jsx
@@ -36,7 +36,7 @@ const Navbar = () => {
                         </NavLink>
                     </div>
                     <div className="flex items-center space-x-4">
-                        <Link to="/cart" className="relative text-gray-500 hover:text-gray-700">
+                        <Link to="/dashboard/cart" className="relative text-gray-500 hover:text-gray-700">
                             <svg xmlns="http://www.w3.org/2000/svg"
                                 className="h-6 w-6"
                                 fill="none"
