199b41435f5120a63039ba47292fda936501def2
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-19 18:32:45 +0600

    unnecessary code removed

--- src/services/auth-api-client.js
+++ src/services/auth-api-client.js
@@ -4,15 +4,10 @@ const authApiClient = axios.create({
     baseURL: "https://drivnbd-serverside.vercel.app/api/v1",
 });
 
-export default authApiClient;
+authApiClient.interceptors.request.use((config) => {
+    const token = JSON.parse(localStorage.getItem("authTokens"))?.access;
+    if (token) config.headers.Authorization = `JWT ${token}`;
+    return config;
+});
 
-authApiClient.interceptors.request.use(
-    (config) => {
-        const token = localStorage.getItem("authTokens");
-        if (token) {
-            config.headers.Authorization = `JWT ${JSON.parse(token)?.access}`;
-        }
-        return config;
-    },
-    (error) => Promise.reject(error)
-);
+export default authApiClient;
