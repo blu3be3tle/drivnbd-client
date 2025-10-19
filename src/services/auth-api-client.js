34154d4492dc0c82aeab95672f48f1f51b8f85a8
Abu Ratin Shoeb <aburatinshoaib@gmail.com>
2025-10-19 18:48:36 +0600

    added console log in authApiClient jwt token

--- src/services/auth-api-client.js
+++ src/services/auth-api-client.js
@@ -4,10 +4,17 @@ const authApiClient = axios.create({
     baseURL: "https://drivnbd-serverside.vercel.app/api/v1",
 });
 
-authApiClient.interceptors.request.use((config) => {
-    const token = JSON.parse(localStorage.getItem("authTokens"))?.access;
-    if (token) config.headers.Authorization = `JWT ${token}`;
-    return config;
-});
+authApiClient.interceptors.request.use(
+    (config) => {
+        const token = localStorage.getItem("authTokens");
+        if (token) {
+            const access = JSON.parse(token)?.access;
+            console.log("JWT token attached:", access?.slice(0, 15));
+            config.headers.Authorization = `JWT ${access}`;
+        }
+        return config;
+    },
+    (error) => Promise.reject(error)
+);
 
 export default authApiClient;
